c6b9d9eefa58fc483d58bc5be5c3194d0d55ee0e
mingrow <nikitosvozh@gmail.com>
2024-11-06 20:23:57 +0300

    Add float getDifference

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import { parse } from '../src/parse.js';
+import parse from '../src/parse.js';
 
 program
   .name('gendiff')
@@ -8,7 +8,10 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filePath1, filePath2, options) => {
-    parse(filePath1, filePath2);
+    if (options.format) {
+      console.log('Hello world!');
+    }
+    console.log(parse(filePath1, filePath2));
   })
   .version('1.0.0');
 
--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,33 @@
 import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
 import path from 'node:path';
+import _ from 'lodash';
 
-export const parse = (firstPath, secondPath) => {
-  const currentDir = cwd();
-  const firstFilePath = path.resolve(currentDir, firstPath);
-  const secondFilePath = path.resolve(currentDir, secondPath);
-  console.log(JSON.parse(readFileSync(firstFilePath)));
-  console.log(JSON.parse(readFileSync(secondFilePath)));
-}
+const parse = (firstPath, secondPath) => {
+  const firstFilePath = path.resolve(cwd(), firstPath);
+  const secondFilePath = path.resolve(cwd(), secondPath);
+  const firstObj = JSON.parse(readFileSync(firstFilePath));
+  const secondObj = JSON.parse(readFileSync(secondFilePath));
+  const keys = _.sortBy(_.union(Object.keys(firstObj), Object.keys(secondObj)));
+  const rsl = keys.reduce((acc, key) => {
+    if (!_.has(firstObj, key) && _.has(secondObj, key)) {
+      return { ...acc, [`+ ${key}`]: secondObj[key] };
+    }
+    if (_.has(firstObj, key) && !_.has(secondObj, key)) {
+      return { ...acc, [`- ${key}`]: firstObj[key] };
+    }
+    if (_.has(firstObj, key) && _.has(secondObj, key)) {
+      if (firstObj[key] !== secondObj[key]) {
+        return {
+          ...acc,
+          [`- ${key}`]: firstObj[key],
+          [`+ ${key}`]: secondObj[key],
+        };
+      }
+    }
+    return { ...acc, [`  ${key}`]: firstObj[key] };
+  }, {});
+  return JSON.stringify(rsl, null, 2).replaceAll('"', '');
+};
+
+export default parse;
