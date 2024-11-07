a56ca9a82894b30f806c71da04701d7e77a6c6df
mingrow <nikitosvozh@gmail.com>
2024-11-07 06:44:24 +0300

    Add first test for parse.js

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -1,3 +1,38 @@
-test('sum', () => {
-  expect(1 + 2).toBe(3);
+ import parse from "../src/parse"
+ 
+ test('Not enough arguments', () => {
+   expect(() => {
+     parse()
+   }).toThrow()
+ });
+
+ test('First file not found', () => {
+   expect(() => {
+     parse('/file1.json', '__fixteres__/file2.json')
+   }).toThrow()
+ });
+
+ test('Second file not found', () => {
+   expect(() => {
+     parse('__fixtures__/file1.json', '/file2.json')
+   }).toThrow()
+ });
+
+ test('Correct arguments', () => {
+   expect(() => {
+     parse('__fixtures__/file1.json', '__fixtures__/file2.json')
+   }).not.toThrow()
+ })
+
+ test('Correct parsing', () => {
+  const result = parse('__fixtures__/file1.json', '__fixtures__/file2.json');
+  expect(result).toBe(
+  `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`)
 })
